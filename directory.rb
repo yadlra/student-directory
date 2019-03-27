810e66bd6bb617d2c84b08c491aa61db5c66d576
yadlra <yadi.sb1@gmail.com>
2019-03-27 19:57:59 +0000

    Interactive Menu

--- directory.rb
+++ directory.rb
@@ -37,10 +37,37 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students".center(25)
 end
 
+def interactive_menu
+  students = []
+  loop do
+    #1.print the menu and ask the user what to do
+      puts "1. Input the students "
+      puts "2. Show the students"
+      puts "9. Exit" #9 because we'll be adding more items
+      #2.read the input and save it into a variable
+      selection = gets.chomp
+      #3. do what the user has asked
+      case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit #this will case the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+      end 
+    end
+  end
+=begin
 students = input_students
 print_header
 print(students)
 print_footer(students)
+=en d
+
 
 =begin
 students = [
