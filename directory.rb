93af8ea55b921270d254d2b359bb49a1984ff4b4
yadlra <yadi.sb1@gmail.com>
2019-03-27 20:37:18 +0000

    Loading the data from the file (12)

--- directory.rb
+++ directory.rb
@@ -41,6 +41,7 @@ def print_menu
   puts "1. Input the students "
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" #9 because we'll be adding more items
 end
 
@@ -58,6 +59,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -84,6 +87,16 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+   @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+#nothing happens until we call the methods
   interactive_menu()
 =begin
 students = input_students
